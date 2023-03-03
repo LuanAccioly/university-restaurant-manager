53ea996f5f72526b694467e21ff1a94de72a8802
Luan Accioly <acciolyluan@gmail.com>
2023-03-03 14:39:42 -0300

    Update Header.js

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -1,22 +1,24 @@
-import { Flex, Heading, useColorModeValue } from "@chakra-ui/react"
-import { Outlet, Link } from "react-router-dom";
-import { ColorModeSwitcher } from "../../ColorModeSwitcher"
+import { Flex, Heading, useColorModeValue } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { ColorModeSwitcher } from '../../ColorModeSwitcher';
 
 export const Header = () => {
-    const outlineColor = useColorModeValue('gray.200', 'gray.700')
+  const outlineColor = useColorModeValue('gray.200', 'gray.700');
 
-    return(
-        <Flex
-            borderBottom={"2px"}
-            borderColor={outlineColor}
-            p={3} 
-            justifyContent={"space-between"} 
-            alignItems={"center"}
-            >
-            <Heading size='md'>UFRPE - RU</Heading >
-            <ColorModeSwitcher/>
-        </Flex>
+  const navigate = useNavigate();
 
-    )
-    
-}
+  return (
+    <Flex
+      borderBottom={'2px'}
+      borderColor={outlineColor}
+      p={3}
+      justifyContent={'space-between'}
+      alignItems={'center'}
+    >
+      <Heading size="md" cursor={'pointer'} onClick={() => navigate('/login')}>
+        UFRPE - RU
+      </Heading>
+      <ColorModeSwitcher />
+    </Flex>
+  );
+};
