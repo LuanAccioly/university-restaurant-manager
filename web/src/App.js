25d57f380d71628062bfc05052683340d85f9881
Luan Accioly <acciolyluan@gmail.com>
2023-03-03 14:38:51 -0300

    Update App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,35 +1,38 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  extendTheme,
-} from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
-import theme from './styles/theme.js'
+import theme from './styles/theme.js';
 import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from 'react-router-dom';
 import { Header } from './components/Header/Header';
+import { Layout } from './pages/Layout';
 
 const router = createBrowserRouter([
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",   
-      element: <Login />,
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/register',
+        element: <Register />,
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+    ],
+  },
+]);
 
-    }
-  ]);
-
-const appTheme = extendTheme(theme) 
+const appTheme = extendTheme(theme);
 function App() {
   return (
     <ChakraProvider theme={appTheme}>
-        <Header/>
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </ChakraProvider>
   );
 }
