b607980c41b4b4ef5059aa81e6b2a5e8a91d7bfa
Luan Accioly <acciolyluan@gmail.com>
2023-03-07 02:46:04 -0300

    Update Home.js

--- web/src/pages/Home/Home.js
+++ web/src/pages/Home/Home.js
@@ -1,21 +1,21 @@
-import {Center, Flex} from "@chakra-ui/react"
-import React, { useContext } from "react";
-import { AuthContext } from "../../contexts/AuthContext";
+import { Center, Flex } from '@chakra-ui/react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import { Menu } from '../Menu/Menu';
 
 export const Home = () => {
-    const {isAuthenticated, user} = useContext(AuthContext)
+  const { isAuthenticated, user } = useContext(AuthContext);
 
-    return(
-
-        <Flex h={isAuthenticated ? "92vh" : '100vh'}>
-            {isAuthenticated ? 
-            (<Center w={"100%"} bg={"pink.200"}>
-                <p>LOGADO {user.name}</p>
-            </Center>) : 
-            (<Center w={"100%"} bg={"pink.200"}>
-                <p>NAO LOGADO</p>
-            </Center>)}
-        </Flex>
-    )
-} 
+  return (
+    <Flex h={isAuthenticated ? '92vh' : '100vh'}>
+      {isAuthenticated ? (
+        <Menu />
+      ) : (
+        <Center w={'100%'} bg={'pink.200'}>
+          <p>NAO LOGADO</p>
+        </Center>
+      )}
+    </Flex>
+  );
+};
 export default Home;
