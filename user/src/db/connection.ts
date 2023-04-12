4254e9bf15a2ec7e27d33c8a3a171f126674d272
Mario Leandro <marioleandro34@gmail.com>
2023-04-12 19:34:57 -0300

    change mongoDB name

--- user/src/db/connection.ts
+++ user/src/db/connection.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 async function main() {
     try {
         mongoose.set("strictQuery", true)
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clustersd.qb2xyaw.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clustersd.qb2xyaw.mongodb.net/?retryWrites=true&w=majority`, {dbName: 'RU'})
         console.log("User connected")
     } catch (error) {
         console.log(error)
